4b0a6f9b3b56922eaa3323fa03f7e8859c811c90
Chris DeCarolis <cdguitar817@gmail.com>
2024-12-17 08:51:23 -0800

    Genericize object list functionality to take in all sensors, jobs, schedules

--- python_modules/dagster/dagster/_core/definitions/module_loaders/utils.py
+++ python_modules/dagster/dagster/_core/definitions/module_loaders/utils.py
@@ -1,19 +1,49 @@
 import pkgutil
 from importlib import import_module
 from types import ModuleType
-from typing import Iterable, Iterator, Mapping, Tuple, Type, Union
+from typing import Any, Iterable, Iterator, Mapping, Tuple, Type, Union
 
 from dagster._core.definitions.asset_key import AssetCheckKey, AssetKey
 from dagster._core.definitions.asset_spec import AssetSpec
 from dagster._core.definitions.assets import AssetsDefinition
 from dagster._core.definitions.cacheable_assets import CacheableAssetsDefinition
+from dagster._core.definitions.executor_definition import ExecutorDefinition
+from dagster._core.definitions.job_definition import JobDefinition
+from dagster._core.definitions.logger_definition import LoggerDefinition
+from dagster._core.definitions.partitioned_schedule import (
+    UnresolvedPartitionedAssetScheduleDefinition,
+)
+from dagster._core.definitions.schedule_definition import ScheduleDefinition
+from dagster._core.definitions.sensor_definition import SensorDefinition
 from dagster._core.definitions.source_asset import SourceAsset
+from dagster._core.definitions.unresolved_asset_job_definition import UnresolvedAssetJobDefinition
+from dagster._core.executor.base import Executor
 
 LoadableAssetObject = Union[AssetsDefinition, AssetSpec, SourceAsset, CacheableAssetsDefinition]
-LoadableDagsterObject = LoadableAssetObject  # For now
+ScheduleDefinitionObject = Union[ScheduleDefinition, UnresolvedPartitionedAssetScheduleDefinition]
+JobDefinitionObject = Union[JobDefinition, UnresolvedAssetJobDefinition]
+ExecutorObject = Union[ExecutorDefinition, Executor]
+LoggerDefinitionKeyMapping = Mapping[str, LoggerDefinition]
+ResourceDefinitionMapping = Mapping[str, Any]
+LoadableDagsterObject = Union[
+    LoadableAssetObject,
+    SensorDefinition,
+    ScheduleDefinitionObject,
+    JobDefinitionObject,
+    ExecutorObject,
+]
 RuntimeKeyScopedAssetObjectTypes = (AssetsDefinition, AssetSpec, SourceAsset)
 RuntimeAssetObjectTypes = (AssetsDefinition, AssetSpec, SourceAsset, CacheableAssetsDefinition)
-RuntimeDagsterObjectTypes = RuntimeAssetObjectTypes  # For now
+RuntimeScheduleObjectTypes = (ScheduleDefinition, UnresolvedPartitionedAssetScheduleDefinition)
+RuntimeJobObjectTypes = (JobDefinition, UnresolvedAssetJobDefinition)
+RuntimeExecutorObjectTypes = (ExecutorDefinition, Executor)
+RuntimeDagsterObjectTypes = (
+    *RuntimeAssetObjectTypes,
+    SensorDefinition,
+    *RuntimeScheduleObjectTypes,
+    *RuntimeJobObjectTypes,
+    *RuntimeExecutorObjectTypes,
+)
 
 
 def find_objects_in_module_of_types(
